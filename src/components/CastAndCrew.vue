b8e68572d5e717a6466379f76acdecd7a5a86f9d
sama-malmir <malmir.sama1@gmail.com>
2023-02-01 13:43:55 +0330

    add showall btn in cast and crew

--- src/components/CastAndCrew.vue
+++ src/components/CastAndCrew.vue
@@ -1,23 +1,30 @@
 <template>
     <div class="ml-[15px]">
         <h2 class="font-bold text-ftm lg:text-ftd">Cast & Crew</h2>
-        <div v-for="bazigar in listeKameleBazigaran" class="flex py-6" >
-            <img :src="bazigar.image"  alt="cast&crow" class="w-w-18 h-h-18 object-cover object-center rounded-full">
-                <div class="flex flex-col justify-center pl-3">
-                     <p class="font-bold text-f16 pb-1.5"> {{ bazigar.name  }}</p>
-                    <p class="opacity-60 font-normal text-f14 ">as {{ bazigar.asCharacter }}</p>
-                </div>
+        <div v-for="bazigar in listeKameleBazigaran" class="flex py-6">
+            <img :src="bazigar.image" alt="cast&crow" class="w-w-18 h-h-18 object-cover object-center rounded-full">
+            <div class="flex flex-col justify-center pl-3">
+                <p class="font-bold text-f16 pb-1.5"> {{ bazigar.name }}</p>
+                <p class="opacity-60 font-normal text-f14 ">as {{ bazigar.asCharacter }}</p>
+            </div>
         </div>
-    </div>
+        <div >
+            <a class="flex justify-center lg:justify-start pt-1.5" href="#">
+                <span class="font-medium text-secondary-color  text-f18 ">Show all</span>
+                <img src="../assets/icon/arrow.svg" alt="show all" class="w-w-21 h-h-21 ml-[15px]">
+            </a>
+        </div>
+        </div>
 </template>
 
 <script>
-export default{
+export default {
     props: {
-       listeKameleBazigaran: Object
+        listeKameleBazigaran: Object
     }
 }
 </script>
 
 <style>
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,18 @@ module.exports = {
       },
       spacing: {
         'w-18': '75px',
+        'w-21':'21px',
       },
       height: {
         'h-18': '75px',
+        'h-21':'21px',
       },
       fontSize: {
         f14: ['14px', '16.41px'],
         f16: ['16px', '18.75px'],
         ftm: ['24px', '50px'],
         ftd: ['28px', '50px'],
+        f18: ['18px','21px'],
       },
       margin: {
         '15px': '15px',
