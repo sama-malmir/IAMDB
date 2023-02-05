f572bda1407a31ec14221c594ec12d2734f6505b
sama-malmir <malmir.sama1@gmail.com>
2023-02-05 21:21:28 +0330

    change photos

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="mx-[15px]">
-        <h2 class="font-bold text-ftm lg:text-ftd">Photos</h2>
+    <div class="mx-[15px] lg:mx-[132px]">
+        <h2 class="font-bold text-ftm lg:text-ftd mb-6">Photos</h2>
         
-            <div class="flex object-cover object-center aspect-w-2 aspect-h-1 gap-2">
-            <img v-for="img in listKameleImgs" :src="img.image" alt="photos" class="rounded-xl  w-1/2 ">
-            </div>
+            <ul class="grid grid-cols-2 gap-3 lg:gap-6 lg:grid-cols-5">
+                <li v-for="img in listKameleImgs" class="  ">
+                    <img  :src="img.image"  alt="photos" class="rounded-xl  w-w-158 h-h-102 object-cover object-center lg:h-h-144 lg:w-w-216">
+                </li>
+            </ul>
     </div>
     <br><br><br>
 </template>
