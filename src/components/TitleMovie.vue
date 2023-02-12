7c6d3c831c5c06aff8ad74a0d9d7d25b50e704f5
sama-malmir <malmir.sama1@gmail.com>
2023-02-12 19:43:10 +0330

    change title movie

--- src/components/TitleMovie.vue
+++ src/components/TitleMovie.vue
@@ -2,7 +2,13 @@
     <div class="ml-[15px] mt-[-90px]">
         <h1 class="text-f32 lg:text-f48 font-bold pb-1.5">{{ esmeFilm }}</h1>
         <p class="text-f16 lg:text-f18 font-normal pb-5	lg:pb-6">Directors: {{ esmeKargardan }}</p>
-        <span class="text-f14 font-normal lg:text-f18">{{ sal }}</span> • <span class="text-f14 font-normal lg:text-f18">{{ baznevisi }}</span> • <span class="text-f14 font-normal lg:text-f18">{{ zamanejra }}</span>
+        <div class="tracking-widest">
+            <span class="text-f14 font-normal lg:text-f18">{{ sal }}</span>
+             •
+            <span class="text-f14 font-normal lg:text-f18">{{ baznevisi }}</span>
+             •
+            <span class="text-f14 font-normal lg:text-f18">{{ zamanejra }}</span>
+        </div>
     </div>
 </template>
 <script>
