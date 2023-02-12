118437b357721d542253e8551cd61427f7d04680
sama-malmir <malmir.sama1@gmail.com>
2023-02-12 19:46:30 +0330

    add package v-viewer

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import 'viewerjs/dist/viewer.css'
+import VueViewer from 'v-viewer'
 import './assets/main.css'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueViewer)
+
 
 app.mount('#app')
