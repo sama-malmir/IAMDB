4ccb7a05ab7031a68f6a26193f746fba8032b098
sama-malmir <malmir.sama1@gmail.com>
2023-02-12 18:25:19 +0330

    add v-viewer and change photos

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
-<template>
-  <body>
+<template >
+  <body >
     <HomeView />
   </body>
 
--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,19 +1,28 @@
 <template>
     <div class="mx-[15px] lg:mx-[132px]">
         <h2 class="font-bold text-ftm lg:text-ftd mb-6">Photos</h2>
-        
-            <ul class="grid grid-cols-2 gap-3 lg:gap-6 lg:grid-cols-5">
-                <li v-for="img in listKameleImgs" class="  ">
-                    <img  :src="img.image"  alt="photos" class="rounded-xl  w-w-158 h-h-102 object-cover object-center lg:h-h-144 lg:w-w-216">
-                </li>
-            </ul>
+        <viewer :images="listKameleImgs">
+            <div class="grid grid-cols-2 gap-3 lg:gap-6 lg:grid-cols-5 ">
+                <img v-for="src in listKameleImgs" :src="src.image" :key="src.image" alt=""
+                    class="rounded-xl w-w-158 h-h-102 object-cover object-center lg:h-h-144 lg:w-w-216 cursor-pointer">
+            </div>
+        </viewer>
     </div>
     <br><br><br>
 </template>
 <script>
-export default{
-    props:{
-        listKameleImgs:Object 
-    }
+
+export default {
+    data() {
+        return {
+        }
+    },
+
+    
+    props: {
+        listKameleImgs: Object
+    },
+    methods: {
+    },
 }
 </script>
