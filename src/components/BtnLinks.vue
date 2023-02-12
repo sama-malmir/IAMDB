cf55b230daed692eab99af3197d7044b93e3c486
sama-malmir <malmir.sama1@gmail.com>
2023-02-12 19:37:32 +0330

    change btn links

--- src/components/BtnLinks.vue
+++ src/components/BtnLinks.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="ml-[15px]">
-        <div class=" w-11 h-11 border-2 rounded-full hover:bg-hover-btnlinks mt-5 lg:w-w-61 lg:h-h-61 ">
-                <img src="../assets/icon/share2.svg" alt="btn link" class="scale-100 p-2 lg:p-4"/>
+    <div class="ml-[15px] inline-flex py-6	">
+        <div class=" w-11 h-11 border-2 rounded-full hover:bg-hover-btnlinks  lg:w-w-61 lg:h-h-61 relative">
+                <img src="../assets/icon/share2.svg" alt="btn link" class="scale-100 absolute bottom-2.5 left-[9px] lg:left-4 lg:bottom-5	"/>
+        </div>
+        <div class=" w-11 h-11 border-2 rounded-full hover:bg-hover-btnlinks lg:w-w-61 lg:h-h-61 ml-4 relative">
+            <img src="../assets/icon/like2.svg" alt="btn link" class="scale-100 absolute bottom-2.5	left-2.5 lg:bottom-[18px] lg:left-[17px]"/>
         </div>
     </div>
 </template>
