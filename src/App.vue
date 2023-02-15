ddd9ec29ae9320d75ab327bc82fb2a388e0bc195
sama-malmir <malmir.sama1@gmail.com>
2023-02-15 17:16:44 +0330

    add not found

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
 <script >
 import { RouterLink, RouterView } from 'vue-router'
 import HomeView from './views/HomeView.vue';
-import Undefined from './views/Undefined.vue';
 </script>
 
 <style scoped>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="grid  max-w-full  h-h-727 place-items-center bg-back-ground py-24 px-6 sm:py-32 lg:px-8">
+        <div class="text-center">
+            <p class="text-main-color opacity-40 font-bold text-8xl lg:text-9xl">404</p>
+            <p class=" text-secondary-color font-bold text-ff40 lg:text-ff48">Movie not found</p>  
+        </div>
+    </main>
+</template>
+<script>
+
+</script>
