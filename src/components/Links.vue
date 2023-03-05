a62e355dff0f0e0fd79bd0ca9751319ffd5e82d9
sama-malmir <malmir.sama1@gmail.com>
2023-03-05 19:33:07 +0330

    change Watch thrailer

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,13 +1,40 @@
 <template>
     <div class="flex ml-[15px] ">
-        <a class="cursor-pointer hover:bg-hover-links" href="#" >
-            <div class="inline-flex bg-secondary-color rounded-full w-44 h-11 lg:w-w-230 lg:h-w-61 items-center justify-around">
+        
+            <button class="cursor-pointer hover:bg-hover-links hover:rounded-full hover:overflow-hidden"  >
+                <a  class="inline-flex bg-secondary-color rounded-full w-44 h-11 lg:w-w-230 lg:h-w-61 items-center justify-around" :href="trailerLink" target="_blank" >
                 <p class="font-normal text-f16 lg:text-f21 ml-3 ">Watch thrailer</p>
                  <img class="w-w-14.66 h-h-18 lg:w-w-17.1 lg:h-h-21 mr-3" src="../assets/icon/play.svg" alt="Watch thrailer" />
-            </div>
-        </a>
+                </a>
+            </button>
+        
     </div>
 </template>
 <script>
-
+export default{
+    data(){
+        return{
+            movieId: null,
+            trailerLink: null
+        }
+    },
+    methods: {
+        getTrailer() {
+            fetch('https://imdb-api.com/en/API/Trailer/k_0m1wyka7/' + this.movieId)
+                .then(response => response.text())
+                .then(response => JSON.parse(response))
+                .then(response => {
+                    this.trailerLink = response.link
+                });
+        }
+    },
+    created(){
+        this.movieId = this.$route.params.movieId
+        this.getTrailer()
+        
+    }
+    
+}
 </script>
+
+ 
