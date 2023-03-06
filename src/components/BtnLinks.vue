342b8e1f30feb80908da986c550bf7b3de838e81
sama-malmir <malmir.sama1@gmail.com>
2023-03-06 11:23:30 +0330

    change btnlink share

--- src/components/BtnLinks.vue
+++ src/components/BtnLinks.vue
@@ -2,10 +2,10 @@
     <div class="ml-[15px] inline-flex">
        <div class="relative">
         <button @click="isOpen=!isOpen" type="button" class=" w-11 h-11 border-2 rounded-full hover:bg-hover-btnlinks  lg:w-w-61 lg:h-h-61  ">
-                <img  src="../assets/icon/share2.svg" target="-blank" alt="btn link" class="scale-100 absolute bottom-2.5 left-[9px] lg:left-4 lg:bottom-5	"/>
+                <img  src="../assets/icon/share2.svg" target="-blank" alt="btn link" class="scale-100 absolute bottom-[11px] left-[10px] lg:left-[18px] lg:bottom-5	"/>
         </button>
         <ul v-if="isOpen" class="list-none flex flex-col w-44 bg-back-ground cursor-pointer absolute bottom-full left-0">
-            <li class="p-3 hover:bg-back-ground opacity-50 ">
+            <li class="p-3 hover:bg-gray-600 opacity-50 ">
                 <s-facebook
             :window-features="windowFeatures"
             :share-options="facebookShareOptions"
@@ -21,7 +21,7 @@
                 </s-facebook>
                 
             </li>
-            <li class="p-3 hover:bg-back-ground opacity-50">
+            <li class="p-3 hover:bg-gray-600 opacity-50">
                 <s-twitter
                     :window-features="windowFeatures"
                     :share-options="twitterShareOptions"
@@ -37,7 +37,7 @@
                 </s-twitter>
                 
             </li>
-            <li class="p-2 hover:bg-back-ground opacity-50">
+            <li class="p-2 hover:bg-gray-600 opacity-50">
                 <s-evernote
                         :window-features="windowFeatures"
                         :share-options="shareOptions"
@@ -52,18 +52,19 @@
                     </div>
             </s-evernote>
             </li>
-            <li class="p-2 hover:bg-back-ground opacity-50">4</li>
-
+            <li class="p-2 hover:bg-gray-600 opacity-50">
+                <button type="button" @click="copyURL()" class="inline-flex gap-3">
+           
+                        <img class="w-7 h-7" src="../assets/icon/icons8-link-24.png" alt="Email Link">
+                        <span class="font-semibold text-xl">Copy Link</span>
+                </button>
+            </li>
         </ul>
        </div>
-    
-    
         </div>
-        <div class=" w-11 h-11 border-2 rounded-full hover:bg-hover-btnlinks lg:w-w-61 lg:h-h-61 ml-4 relative">
-            <img src="../assets/icon/like2.svg" alt="btn link" class="scale-100 absolute bottom-2.5	left-2.5 lg:bottom-[18px] lg:left-[17px]"/>
+        <div class=" w-11 h-11 border-2 rounded-full hover:bg-hover-btnlinks  lg:w-w-61 lg:h-h-61 ml-4 relative">
+            <img src="../assets/icon/like2.svg" alt="btn link" class="scale-100 absolute bottom-[9px]	left-[9px] lg:bottom-[18px] lg:left-[17px]"/>
         </div>
-    
-
 </template>
 <script>
 export default{
@@ -72,31 +73,17 @@ export default{
       isOpen: false
     }
   },
-//     data () {
-        
-//     return {
-//       windowFeatures: {},
-//       facebookShareOptions: {
-//         url: 'https://github.com/',
-//         quote: 'Quote',
-//         hashtag: '#Github',
-//       },
-//       twitterShareOptions: {
-//         url: 'https://github.com/',
-//         text: 'Hello world',
-//         hashtags: ['hash', 'tag'],
-//         via: 'twitterdev',
-//       },
-//       useNativeBehavior: false,
-//     }
-    
-//   },
   methods:{
-    
     onClose() {},
     onOpen() {},
     onBlock() {},
     onFocus() {},
-  }
+    copyURL() {
+        navigator.clipboard.writeText('http://localhost:5173/' + this.movieId);
+    }
+  },
+  created() {
+    this.movieId = this.$route.params.movieId
+  },
 }
 </script>
